b6305bea9aa6f2c24807d83073119d0885f1a2a4
laurahoffmeyer <lhoffmeyer89@gmail.com>
2021-09-07 20:57:03 -0400

    updating read me, adjustment to footer

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
 # ReactTipCalculator
 Small Tip Calculator App built with React. Challange provided by FrontendMenor.
-=======
-Small Tip Calculator App built with React. Challenge pulled from FrontendMenor.io
->>>>>>> 2c2061d40746adb20546716f505bf53b5c135995
+
+Live preview here - https://react-tip-calculator-lh.netlify.app/
--- src/componets/App/App.js
+++ src/componets/App/App.js
@@ -6,14 +6,13 @@ import React, {useState} from 'react';
 function App() {
   const [calculatorId, setCalculatorId] = useState(1);
 
-  console.log(calculatorId);
   return (
     <div className="app">
       <header>
         <img alt="SPLITTER logo" src={logo} />
       </header>
       <Calculator key={calculatorId} resetCalculator={() => setCalculatorId(calculatorId + 1)} />
-      <footer>Made with ♥ in Pontiac, MI by <a target="_blank" href="http://laurahoffmeyer.com">Laura Hoffmeyer</a></footer>
+      <footer>Made with ♥ in Pontiac, MI by <a rel="noreferrer" target="_blank" href="http://laurahoffmeyer.com">Laura Hoffmeyer</a></footer>
     </div>
   );
 }
